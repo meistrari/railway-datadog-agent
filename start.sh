e07c5bfd4ab9c5437de16c5e3593e358a0939ad8
Matheus Peviani Vellone <matheusvellone@gmail.com>
2025-09-15 14:41:01 -0300

    feat: add public collector configuration and update start script

--- start.sh
+++ start.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 
 CONFIG_FILE="/etc/datadog-agent/otel-config.yaml"
 
+IS_PUBLIC_COLLECTOR=${IS_PUBLIC_COLLECTOR:-"false"}
+
+if [ "$IS_PUBLIC_COLLECTOR" = "true" ]; then
+  CONFIG_FILE="/etc/datadog-agent/otel-config-public.yaml"
+fi
+
 echo "Using config file: $CONFIG_FILE"
 
 otel-agent --config=$CONFIG_FILE --core-config=/etc/datadog-agent/datadog.yaml
