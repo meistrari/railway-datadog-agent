7ef5f7da7251be75c1575346f3d79240e23a6d54
Matheus Peviani Vellone <matheusvellone@gmail.com>
2025-09-15 14:45:19 -0300

    feat: update Dockerfile and scripts to use new configuration structure for Datadog agent

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-CONFIG_FILE="/etc/datadog-agent/otel-config.yaml"
+CONFIG_FILE="/otel-config/otel-config-private.yaml"
 
 IS_PUBLIC_COLLECTOR=${IS_PUBLIC_COLLECTOR:-"false"}
 
 if [ "$IS_PUBLIC_COLLECTOR" = "true" ]; then
-  CONFIG_FILE="/etc/datadog-agent/otel-config-public.yaml"
+  CONFIG_FILE="/otel-config/otel-config-public.yaml"
 fi
 
 echo "Using config file: $CONFIG_FILE"
